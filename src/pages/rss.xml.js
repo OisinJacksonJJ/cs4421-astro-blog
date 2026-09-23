8ef87b9395c8a2fdd93e96b7378e808fb5eeb2bd
Oisín Jackson <oisin.jackson08@gmail.com>
2026-09-23 15:10:42 +0100

    fix: updated rss in accordance to CI needs

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,9 +8,11 @@ export async function GET(context) {
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
+		items: posts.map(({ id, data }) => ({
+			title: data.title,
+			description: data.description,
+			pubDate: data.pubDate,
+			link: `/blog/${id}/`,
 		})),
 	});
 }
